0c4f863993c6969cb795c71272ed8bf26857d41c
Mikkel <freddyhighmore@mail.ru>
2023-12-23 02:47:01 +0600

    add Example 005 Condition If-Else

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mikkel")
+{
+    Console.WriteLine("Привет мой пупсик:*");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
